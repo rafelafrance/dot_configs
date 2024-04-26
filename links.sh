1387a2df055228f5a1848ac4cd54266a87f4e6df
rafelafrance <raphael.lafrance@gmail.com>
2024-04-26 15:11:20 -0400

    Add more configs and add a script to make links

---
+++ links.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd $HOME/.config
+
+ln -s dot_config/nvim nvim
+ln -s dot_config/tmux tmux
+
+ln -s dot_config/kitty kitty
+ln -s dot_config/wezterm wezterm
+ln -s dot_config/alacritty alacritty
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+return {
+	font = wezterm.font 'FiraCode Nerd Font',
+	font_size = 13,
+	color_scheme = "OneDark (base16)",
+	-- color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
+	hide_tab_bar_if_only_one_tab = true,
+	-- enable_kitty_keyboard = true,
+}
