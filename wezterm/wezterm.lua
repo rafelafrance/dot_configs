3fceebb402392b38a99796905cba87dd23d612df
rafelafrance <raphael.lafrance@gmail.com>
2024-09-01 09:27:23 -0400

    Minor updates to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,13 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 return {
-	font = wezterm.font 'FiraCode Nerd Font',
+	font = wezterm.font("FiraCode Nerd Font"),
 	font_size = 13,
 	color_scheme = "OneDark (base16)",
-	-- color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte
 	hide_tab_bar_if_only_one_tab = true,
+	window_decorations = "RESIZE",
 	-- enable_kitty_keyboard = true,
+	-- adjust_window_size_when_changing_font_size = false,
+	-- use_fancy_tab_bar = false,
+	-- tab_bar_at_bottom = false,
+	-- enable_tab_bar = false,
 }
