1225c3d03b93f04d7e074df2f1ab5ebde9e87d35
rafelafrance <raphael.lafrance@gmail.com>
2025-01-01 11:34:43 -0500

    Use mini's pairs module

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"echasnovski/mini.nvim",
+		config = function()
+			-- require("mini.ai").setup()
+			-- require("mini.align").setup()
+			-- require("mini.animate").setup()
+			-- require("mini.bracketed").setup()
+			-- require("mini.clue").setup()
+			-- require("mini.comment").setup()
+			-- require("mini.completion").setup()
+			-- require("mini.files").setup()
+			-- require("mini.operators").setup()
+			require("mini.pairs").setup()
+			-- require("mini.statusline").setup()
+			-- require("mini.surround").setup()
+		end,
+	},
+}
