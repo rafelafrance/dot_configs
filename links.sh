62d1040802b574c45da568ddcf269b7aa8d41b64
rafelafrance <raphael.lafrance@gmail.com>
2025-04-13 16:55:26 -0400

    Add helix editor settings

--- links.sh
+++ links.sh
@@ -4,6 +4,7 @@ cd ..
 
 ln -s dot_configs/nvim nvim
 ln -s dot_configs/tmux tmux
+ln -s dot_configs/helix helix
 
 ln -s dot_configs/kitty kitty
 ln -s dot_configs/wezterm wezterm
@@ -11,4 +12,3 @@ ln -s dot_configs/wezterm wezterm
 ln -s dot_configs/zls/zls.json zls.json
 
 ln -s dot_configs/starship/starship.toml starship.toml
-
